9e6577ac989959370332b1d520de13f9b846600c
senma <senma@SentekiMacBook-Pro.local>
2014-03-05 21:25:33 +0800

    Initial Commit

---
+++ ControlTheBalloon/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  ControlTheBalloon
+//
+//  Created by Sen Ma on 14-3-5.
+//  Copyright Sen Ma 2014年. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
